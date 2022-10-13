54f7f3f1d8f6ec54052fde8dbbcb7f91c56b3edf
RiteshtheSharma <ritesh0sharma0002@gmail.com>
2022-10-13 18:03:04 +0530

    q1

--- Question 1.py
+++ Question 1.py
@@ -1,6 +1,6 @@
 from functools import reduce
 
-
+# using reduce function to find the some
 def findSum(arr):
     return reduce(lambda a, b: a+b, arr)
 
@@ -8,6 +8,8 @@ def findSum(arr):
 
 if __name__ == "__main__":
     size = int(input())
+
+    # function to verify whether the strings in the array are digits or not 
     def verify(val):
         try:
             return int(val)
@@ -15,6 +17,8 @@ if __name__ == "__main__":
             print(f"Array can only contain numbers. Found '{val}'")
             exit()
     try:
+        # input().split() will take input and split the string by its white spaces 
+        # Using map function to apply validation over every splitted string 
         arr = list(map(verify, input().split()))
     except:
         exit()
--- Question 2.py
+++ Question 2.py
@@ -1,9 +1,11 @@
 from functools import reduce
 
+
 def find_largest(arr):
     return reduce(lambda a,b:a if a>b else b,arr)
 
 def find_lowest_positive_integer(arr):
+    # finding largest in the array 
     for v in range(1,find_largest(arr)+2):
         if v in arr:
             continue
