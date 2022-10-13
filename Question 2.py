0a4b4bd733d447d409562e3fffa6216823c91007
RiteshtheSharma <ritesh0sharma0002@gmail.com>
2022-10-13 18:10:06 +0530

    comm

--- Question 2.py
+++ Question 2.py
@@ -5,8 +5,10 @@ def find_largest(arr):
     return reduce(lambda a,b:a if a>b else b,arr)
 
 def find_lowest_positive_integer(arr):
-    # finding largest in the array 
+    # finding largest in the array to loop through and
+    #  adding 2 because array might contain all possible positive integer 
     for v in range(1,find_largest(arr)+2):
+        # checking if v exists in the array, if not then we got our lowest positive integer 
         if v in arr:
             continue
         else:
